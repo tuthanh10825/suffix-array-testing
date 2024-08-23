971409a698db295fa3dd3b0d20f4096147436da4
Tu Thanh <tuthanh10825@gmail.com>
2024-08-23 22:58:50 +0700

    fix: improve run time.

--- suffix-array/Header.h
+++ suffix-array/Header.h
@@ -3,6 +3,7 @@
 #include <string>
 #include <numeric>
 #include <algorithm>
+#include <iostream>
 using std::vector; 
 class suffix_arr
 {
@@ -14,20 +15,23 @@ public:
 		int n = text.size(); 
 		SA_index.resize(n); 
 		std::iota(SA_index.begin(), SA_index.end(), 0); 
-		std::sort(SA_index.begin(), SA_index.end(), [this](int a, int b) { return text[a] < text[b];  }); 
-		vector<int> primary_key(n); int value = 1; primary_key[SA_index[0]] = 1; 
-		vector<bool> mark(n, 0); mark[0] = true; 
+		std::sort(SA_index.begin(), SA_index.end(), [this ](int a, int b) { return text[a]  < text[b];  }); 
+		
+		
+		vector<int> primary_key(n); int value = 0; primary_key[SA_index[0]] = 0; 
+		vector<bool> mark(n, false); mark[0] = true; 
 
 		for (int i = 1; i < n; ++i)
 		{
-			if (text[SA_index[i]] > text[SA_index[i - 1]])
+			if (text[SA_index[i]] != text[SA_index[i - 1]])
 			{
 				++value;
-				mark[i] = false;
+				mark[i] = true;
 			}
-			else mark[i] = true; 
+			else mark[i] = false; 
 			primary_key[SA_index[i]] = value;
 		}
+		if (value == n - 1) return; 
 		vector<int> temp_SA_index;
 		int k = 1;
 		
@@ -37,8 +41,22 @@ public:
 			for (int i = 0; i < n; ++i)
 				temp_SA_index[i] = (SA_index[i] + n - k) % n;
 					
+			vector <int> cou(10, 0);
+			vector <int> temp_vector(n);
+			for (int count = 1; count <= n; count *= 10000)
+			{
+				temp_vector.resize(n);
+				std::fill(cou.begin(), cou.end(), 0);
+				for (int i = 0; i < n; ++i)
+					++cou[primary_key[temp_SA_index[i]] / count % 10000];
+				for (int i = 1; i < 10000; ++i)
+					cou[i] += cou[i - 1];
+				for (int i = n - 1; i >= 0; --i)
+					temp_vector[--cou[primary_key[temp_SA_index[i]] / count % 10000]] = temp_SA_index[i];
+				temp_SA_index = std::move(temp_vector);
+			}
 
-			std::stable_sort(temp_SA_index.begin(), temp_SA_index.end(), [primary_key](int a, int b) {return primary_key[a] < primary_key[b]; }); 
+			
 			SA_index = std::move(temp_SA_index); 
 			for (int i = 1; i < n; ++i)
 			{
--- suffix-array/main.cpp
+++ suffix-array/main.cpp
@@ -1,12 +1,44 @@
 #include "Header.h"
 #include <iostream>
+#include <random>
+#include <chrono>
+#include <cstdlib>
 using namespace std; 
 
 int main()
 {
-	std::string test = "suffixarraytest@";
+	std::string test; 
+	std::random_device rd; 
+	std::mt19937 rng(rd());
+	std::uniform_int_distribution<int> unidist(0,25); 
+	int n; std::cin >> n;
+	for (int i = 0; i < n; ++i)
+		test += (unidist(rng) + 'a'); 
+	test.push_back('@'); 
+	//std::cout << "string value: " << test << "\n"; 
+	
+	auto start = std::chrono::high_resolution_clock::now();
 	suffix_arr t; t.text = test; 
-	t.build(); 
+	t.build();
+	auto end = std::chrono::high_resolution_clock::now();
+	std::cout << "Time used: " << std::chrono::duration_cast<std::chrono::seconds>(end - start).count();
+	string prev; 
+	string curr; 
+	
 	for (int i = 0; i < t.text.size(); ++i)
-		std::cout << t.text.substr(t.SA_index[i]) << " ";
+	{
+		curr = t.text.substr(t.SA_index[i]); 
+		try
+		{
+			if (prev > curr) throw("Wrong order.");
+		}
+		catch (const char* error)
+		{
+			std::cout << error << " "; 
+			return -1; 
+		}
+		prev = curr; 
+		//std::cout << t.text.substr(t.SA_index[i]) << " ";
+	}
+	return 0; 
 }
